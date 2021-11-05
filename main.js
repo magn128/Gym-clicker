d547c53162f1443ea1527e0ba97451700cc8d5d5
Fredrik Elias Freding <fredrikfreding04@gmail.com>
2021-11-05 13:29:29 +0100

    forslag

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 var i = 0;
 var x = 1;
 var img = 1;
+var testo = 0;
+var boughta = false;
+var boughtp = false;
+var boughtpw = false;
 function function1() {   
-i = i + x
+i = i + x + testo;
 
-document.getElementById("score").innerHTML = i
-document.getElementById("")
+document.getElementById("score").innerHTML = "Du har " + i + " penger!"
+document.getElementById("testo").innerHTML = "Du har " + testo + " testosteron!"
 }
+
+function airpods() {
+    if (boughta === false) {
+        testo = testo + 20
+    }
+    boughta = true;
+    document.getElementById("airpods_mld").innerHTML = "Du har kjøpt airpods!";
+}
+function protein() {
+    if (boughtp === false) {
+        testo = testo + 5
+    }
+    boughtp = true;
+    document.getElementById("protein_mld").innerHTML = "Du har kjøpt protein!";
+}
+function pwo() {
+    if (boughtpw === false) {
+        testo = testo + 10
+    }
+    boughtpw = true;
+    document.getElementById("pwo_mld").innerHTML = "Du har kjøpt PWO!";
+}
+
+
+
