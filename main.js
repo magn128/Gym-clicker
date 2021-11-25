e9894d86116e840a126e6afa81b14144e9fc38c4
magn128 <Magnus@wikerholmen.no>
2021-11-25 09:37:27 +0100

    made upgrade
    la til roids og viser hvor mange ganger man har kjøpt oppgradering

--- main.js
+++ main.js
@@ -1,20 +1,104 @@
-var i = 0;
+var version = 3;
+var penger = 0;
 var x = 1;
 var img = 1;
 var testo = 0;
-var boughta = false;
-var boughtp = false;
-var boughtpw = false;
+var boughta = 0;
+var boughtp = 0;
+var boughtpw = 0;
+var boughtr = 0;
 var pwo_pris = 100;
 var protein_pris = 500;
 var airpods_pris = 1500;
 var roids_pris = 5000;
 var passiv = 0;
-var boughtr = false;
+var roids_power = 0;
 
+
+window.onload = function load() {
+    if(parseInt(localStorage.getItem("saved")) === version){
+    if(localStorage.getItem("saved") !== null){
+     
+     if(localStorage["saved"] !== undefined){
+     
+         if(typeof(localStorage.getItem("saved")) === "string"){
+         penger = parseInt(localStorage.getItem("penger"));
+         x = parseInt(localStorage.getItem("x"));
+         img = parseInt(localStorage.getItem("img"));
+         testo = parseInt(localStorage.getItem("testo"));
+         boughta = parseInt(localStorage.getItem("boughta"));
+         boughtp = parseInt(localStorage.getItem("bougthp"));
+         boughtpw = parseInt(localStorage.getItem("boughtpw"));
+         pwo_pris = parseInt(localStorage.getItem("pwo_pris"));
+         protein_pris = parseInt(localStorage.getItem("protein_pris"));
+         airpods_pris = parseInt(localStorage.getItem("airpods_pris"));
+         roids_pris = parseInt(localStorage.getItem("roids_pris"));
+         passiv = parseInt(localStorage.getItem("passiv"));
+         boughtr = parseInt(localStorage.getItem("boughtr"));
+        roids_power = parseInt(localStorage.getItem("roids_power"));
+        console.log("Loaded!");
+ }
+  }
+  }
+  }
+  }
+
+  var bb = setInterval(save,10000);
+  function save() {
+      localStorage.setItem("saved", version);
+      localStorage.setItem("penger", penger);
+      localStorage.setItem("x", x);
+      localStorage.setItem("img", img);
+      localStorage.setItem("testo", testo);
+      localStorage.setItem("boughta", boughta);
+      localStorage.setItem("boughtp", boughtp);
+      localStorage.setItem("boughtpw", boughtpw);
+      localStorage.setItem("boughtr", boughtr);
+      localStorage.setItem("pwo_pris", pwo_pris);
+      localStorage.setItem("protein_pris", protein_pris);
+      localStorage.setItem("airpods_pris", airpods_pris);
+      localStorage.setItem("roids_pris", roids_pris);
+      localStorage.setItem("passiv", passiv);
+      localStorage.setItem("roids_power", roids_power);
+  
+      console.log("Saved");
+  
+  }
+function reset() {
+    penger = 0;
+    x = 1;
+    img = 1;
+    testo = 0;
+    boughta = 0;
+    boughtp = 0;
+    boughtpw = 0;
+    boughtr = 0;
+    pwo_pris = 100;
+    protein_pris = 500;
+    airpods_pris = 1500;
+    roids_pris = 5000;
+    passiv = 0;
+    roids_power = 0;
+    localStorage.setItem("saved", version);
+      localStorage.setItem("penger", penger);
+      localStorage.setItem("x", x);
+      localStorage.setItem("img", img);
+      localStorage.setItem("testo", testo);
+      localStorage.setItem("boughta", boughta);
+      localStorage.setItem("boughtp", boughtp);
+      localStorage.setItem("boughtpw", boughtpw);
+      localStorage.setItem("boughtr", boughtr);
+      localStorage.setItem("pwo_pris", pwo_pris);
+      localStorage.setItem("protein_pris", protein_pris);
+      localStorage.setItem("airpods_pris", airpods_pris);
+      localStorage.setItem("roids_pris", roids_pris);
+      localStorage.setItem("passiv", passiv);
+      localStorage.setItem("roids_power", roids_power);
+    window.location.reload();
+}
 
 function function1() {   
-    i = i + x + testo;
+    penger = penger + x + testo;
     new Audio("sound/Recording (3).m4a").play();
     
 
@@ -22,68 +106,77 @@ function function1() {
 }
 
 function airpods() {
-    if (boughta === false) {
-        if (i >= airpods_pris) {
-        testo = testo + 20
-        boughta = true;
-        document.getElementById("airpods_mld").innerHTML = "Du har kjøpt airpods!";
-        new Audio("sound/pump.mp3").play();
+
+        if (penger >= airpods_pris) {
+            penger = penger - airpods_pris
+            airpods_pris *= 2;
+            testo += 20;
+            boughta += 1;
+            document.getElementById("airpods_mld").innerHTML = "Du har kjøpt airpods!";
+            new Audio("sound/pump.mp3").play();
     
     }
-    
-}
     
 }
 function protein() {
-    if (boughtp === false) {
-        if (i >= protein_pris) {
-        testo = testo + 10
-        boughtp = true;
-        document.getElementById("protein_mld").innerHTML = "Du har kjøpt protein pulver!";
-        new Audio("sound/protein.mp3").play();
-    }
-}
+
+        if (penger >= protein_pris) {
+            penger = penger - protein_pris
+            protein_pris *= 1.1;
+            testo += 10;
+            boughtp = 1;
+            document.getElementById("protein_mld").innerHTML = "Du har kjøpt protein pulver!";
+            new Audio("sound/protein.mp3").play();
+        }
 
 }
 function pwo() {
-    if (boughtpw === false) {
+   
         
-        if (i >= pwo_pris) {
-            i = i - pwo_pris
-            testo = testo + 2
-            boughtpw = true;
+        if (penger >= pwo_pris) {
+            penger = penger - pwo_pris
+            pwo_pris *= 1.1;
+            testo += 2;
+            boughtpw += 1;
             document.getElementById("pwo_mld").innerHTML = "Du har kjøpt PWO!";
             //takk noa
             new Audio("sound/lightweight_baby.mp3").play();
             
         }
-    }
+    
     
     
 }
 function roids(){
-    if (boughtr === false) {
-        if ( i >= roids_pris) {
-            i = i - roids_pris
-            
+
+        if ( penger >= roids_pris) {
+            penger = penger - roids_pris
+            roids_pris*= 2;
+            testo += 10;
+            roids_power += 10
+            document.getElementById("roids_mld").innerHTML = "Du har kjøpt roids!";
         }
-    }
 }
 
 setInterval(function(){
-    document.getElementById("score").innerHTML = "Du har " + i + " penger!";
-    document.getElementById("testo").innerHTML = "Du har " + testo + " testosteron!";
+    document.getElementById("score").innerHTML = "Du har " + Math.floor(penger) + " penger!";
+    document.getElementById("testo").innerHTML = "Du har " + Math.floor(testo) + " testosteron!";
     
     //clicks++;
 }, 10);
 setInterval(function(){
-    document.getElementById("pwo_pris").innerHTML = pwo_pris;
-    document.getElementById("protein_pris").innerHTML = protein_pris;
-    document.getElementById("airpods_pris").innerHTML = airpods_pris;
+    document.getElementById("pwo_pris").innerHTML = Math.floor(pwo_pris);
+    document.getElementById("protein_pris").innerHTML = Math.floor(protein_pris);
+    document.getElementById("airpods_pris").innerHTML = Math.floor(airpods_pris);
+    document.getElementById("roids_pris").innerHTML = Math.floor(roids_pris)
+    document.getElementById("pwo_mengde").innerHTML = Math.floor(boughtpw)
+    document.getElementById("protein_mengde").innerHTML = Math.floor(boughtp)
+    document.getElementById("airpods_mengde").innerHTML = Math.floor(boughta)
+    document.getElementById("roids_mengde").innerHTML = Math.floor(boughtr)
 
-}, 1000);
+}, 10);
 setInterval(function() {
-
+    penger += roids_power
 
 }, 1000);
 
