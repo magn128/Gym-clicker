079544a90bb959c4fb425efbbb3a5f6d199b8169
magn128 <72791503+magn128@users.noreply.github.com>
2021-11-26 21:57:38 +0100

    la til ting
    la til en ny kjøpbar oppdatering og laget en fil som er for å oppbevare kode som ikke skal brukes enda

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 var version = 4;
 var penger = 0;
+var formue = 0;
 var x = 1;
 var img = 1;
 var testo = 0;
 var boughta = 0;
 var boughtp = 0;
 var boughtpw = 0;
-var boughtr = 0;
+var boughtroids = 0;
 var boughtbelt = 0;
 var boughtsarms = 0;
+var boughtsleeves = 0;
 var pwo_pris = 100;
 var protein_pris = 500;
 var airpods_pris = 1500;
 var roids_pris = 5000;
 var belte_pris = 100000;
 var sarms_pris = 200000;
+var sleeves_pris = 1000000;
 var passiv = 0;
 var power = 0;
 var gold_dumbbell = false;
 var max = 0;
 var min = 0;
+
 
 
 // Fredrik hjalp med dette
@@ -68,6 +72,7 @@ function golden_dumbell() {
     } else {
         var random_peng = Math.floor(Math.random() * (max - min + 1)) + min;
         penger = penger + random_peng;
+        formue = formue + random_peng
         // Kopiert fra StackOverflow
         var myNode = document.getElementById("1");
         while (myNode.firstChild) {
@@ -94,25 +99,30 @@ window.onload = function load() {
      
      if(localStorage["saved"] !== undefined){
      
-         if(typeof(localStorage.getItem("saved")) === "string"){
-         penger = parseInt(localStorage.getItem("penger"));
-         x = parseInt(localStorage.getItem("x"));
-         img = parseInt(localStorage.getItem("img"));
-         testo = parseInt(localStorage.getItem("testo"));
-         boughta = parseInt(localStorage.getItem("boughta"));
-         boughtp = parseInt(localStorage.getItem("boughtp"));
-         boughtpw = parseInt(localStorage.getItem("boughtpw"));
-         pwo_pris = parseInt(localStorage.getItem("pwo_pris"));
-         protein_pris = parseInt(localStorage.getItem("protein_pris"));
-         airpods_pris = parseInt(localStorage.getItem("airpods_pris"));
-         belte_pris = parseInt(localStorage.getItem("belte_pris"));
-         roids_pris = parseInt(localStorage.getItem("roids_pris"));
-         passiv = parseInt(localStorage.getItem("passiv"));
-         boughtr = parseInt(localStorage.getItem("boughtr"));
-         boughtbelt = parseInt(localStorage.getItem("boughtbelt"));
-        power = parseInt(localStorage.getItem("power"));
-        console.log("Loaded!");
- }
+        if(typeof(localStorage.getItem("saved")) === "string"){
+            penger = parseInt(localStorage.getItem("penger"));
+            formue = parseInt(localStorage.getItem("formue"));
+            x = parseInt(localStorage.getItem("x"));
+            img = parseInt(localStorage.getItem("img"));
+            testo = parseInt(localStorage.getItem("testo"));
+            boughta = parseInt(localStorage.getItem("boughta"));
+            boughtp = parseInt(localStorage.getItem("boughtp"));
+            boughtpw = parseInt(localStorage.getItem("boughtpw"));
+            pwo_pris = parseInt(localStorage.getItem("pwo_pris"));
+            protein_pris = parseInt(localStorage.getItem("protein_pris"));
+            airpods_pris = parseInt(localStorage.getItem("airpods_pris"));
+            belte_pris = parseInt(localStorage.getItem("belte_pris"));
+            roids_pris = parseInt(localStorage.getItem("roids_pris"));
+            sarms_pris = parseInt(localStorage.getItem("sarms_pris"));
+            sleeves_pris = parseInt(localStorage.getItem("sleeves_pris"));
+            passiv = parseInt(localStorage.getItem("passiv"));
+            boughtroids = parseInt(localStorage.getItem("boughtroids"));
+            boughtbelt = parseInt(localStorage.getItem("boughtbelt"));
+            boughtsarms = parseInt(localStorage.getItem("boughtsarms"));
+            boughtsleeves = parseInt(localStorage.getItem("boughtsleeves"));
+            power = parseInt(localStorage.getItem("power"));
+            console.log("Loaded!");
+    }
   }
   }
   }
@@ -122,19 +132,24 @@ window.onload = function load() {
   function save() {
       localStorage.setItem("saved", version);
       localStorage.setItem("penger", penger);
+      localStorage.setItem("formue", formue);
       localStorage.setItem("x", x);
       localStorage.setItem("img", img);
       localStorage.setItem("testo", testo);
       localStorage.setItem("boughta", boughta);
       localStorage.setItem("boughtp", boughtp);
       localStorage.setItem("boughtpw", boughtpw);
-      localStorage.setItem("boughtr", boughtr);
+      localStorage.setItem("boughtroids", boughtroids);
       localStorage.setItem("boughtbelt", boughtbelt);
+      localStorage.setItem("boughtsarms", boughtsarms);
+      localStorage.setItem("boughtsleeves", boughtsleeves);
       localStorage.setItem("belte_pris", belte_pris);
       localStorage.setItem("pwo_pris", pwo_pris);
       localStorage.setItem("protein_pris", protein_pris);
       localStorage.setItem("airpods_pris", airpods_pris);
       localStorage.setItem("roids_pris", roids_pris);
+      localStorage.setItem("sarms_pris", sarms_pris);
+      localStorage.setItem("sleeves_pris", sleeves_pris);
       localStorage.setItem("passiv", passiv);
       localStorage.setItem("power", power);
   
@@ -143,44 +158,55 @@ window.onload = function load() {
   }
 function reset() {
     penger = 0;
+    formue = 0;
     x = 1;
     img = 1;
     testo = 0;
     boughta = 0;
     boughtp = 0;
     boughtpw = 0;
-    boughtr = 0;
+    boughtroids = 0;
     boughtbelt = 0;
+    boughtsarms = 0;
+    boughtsleeves = 0;
     pwo_pris = 100;
     protein_pris = 500;
     airpods_pris = 1500;
     roids_pris = 5000;
     belte_pris = 100000;
+    sarms_pris = 200000;
+    sleeves_pris = 1000000;
     passiv = 0;
     power = 0;
     
     localStorage.setItem("saved", version);
-      localStorage.setItem("penger", penger);
-      localStorage.setItem("x", x);
-      localStorage.setItem("img", img);
-      localStorage.setItem("testo", testo);
-      localStorage.setItem("boughta", boughta);
-      localStorage.setItem("boughtp", boughtp);
-      localStorage.setItem("boughtpw", boughtpw);
-      localStorage.setItem("boughtr", boughtr);
-      localStorage.setItem("boughtbelt", boughtbelt);
-      localStorage.setItem("pwo_pris", pwo_pris);
-      localStorage.setItem("protein_pris", protein_pris);
-      localStorage.setItem("airpods_pris", airpods_pris);
-      localStorage.setItem("roids_pris", roids_pris);
-      localStorage.setItem("belte_pris", belte_pris)
-      localStorage.setItem("passiv", passiv);
-      localStorage.setItem("power", power);
+    localStorage.setItem("penger", penger);
+    localStorage.setItem("formue", formue);
+    localStorage.setItem("x", x);
+    localStorage.setItem("img", img);
+    localStorage.setItem("testo", testo);
+    localStorage.setItem("boughta", boughta);
+    localStorage.setItem("boughtp", boughtp);
+    localStorage.setItem("boughtpw", boughtpw);
+    localStorage.setItem("boughtroids", boughtroids);
+    localStorage.setItem("boughtbelt", boughtbelt);
+    localStorage.setItem("boughtsarms", boughtsarms);
+    localStorage.setItem("boughtsleeves", boughtsleeves);
+    localStorage.setItem("pwo_pris", pwo_pris);
+    localStorage.setItem("protein_pris", protein_pris);
+    localStorage.setItem("airpods_pris", airpods_pris);
+    localStorage.setItem("roids_pris", roids_pris);
+    localStorage.setItem("belte_pris", belte_pris);
+    localStorage.setItem("sarms_pris", sarms_pris);
+    localStorage.setItem("sleeves_pris", sleeves_pris);
+    localStorage.setItem("passiv", passiv);
+    localStorage.setItem("power", power);
     window.location.reload();
 }
 
 function function1() {   
     penger = penger + x + testo;
+    formue = formue + x + testo;
     new Audio("sound/Recording (3).m4a").play();
     
 
@@ -233,10 +259,10 @@ function roids(){
 
         if ( penger >= roids_pris) {
             penger = penger - roids_pris
-            roids_pris*= 1.5;
+            roids_pris*= 1.2;
             testo += 10;
-            boughtr += 1;
-            power = boughtr *12;
+            boughtroids += 1;
+            power = boughtroids *12;
             document.getElementById("roids_mld").innerHTML = "Du har kjøpt roids!";
             new Audio("sound/").play();
         }
@@ -260,20 +286,28 @@ function sarms() {
         sarms_pris *= 1.5;
         testo += 10;
         boughtsarms += 1;
-        power = boughtsarms *20;
+        power = boughtsarms *1500;
         document.getElementById("sarms_mld".innerHTML = "Du har kjøpt sarms!")
         new Audio("sound/").play();
     }
 }
 
+function sleeves() {
 
-
-
+    if (penger >= sleeves_pris) {
+        penger = penger - sleeves_pris
+        sleeves_pris *= 1.3;
+        testo += 1000;
+        boughtsleeves += 1;
+        document.getElementById("sleeves_mld").innerHTML = "Du har kjøpt Knee sleeves!";
+        new Audio("sound/").play();
+    }
+}
 
 setInterval(function(){
     document.getElementById("score").innerHTML = "Du har " + Math.floor(penger) + " penger!";
     document.getElementById("testo").innerHTML = "Du har " + Math.floor(testo) + " testosteron!";
-    
+    document.getElementById("formue").innerHTML = "Du har tjent " + Math.floor(formue) + " penger i karrieren din!"
     //clicks++;
 }, 10);
 setInterval(function(){
@@ -283,16 +317,18 @@ setInterval(function(){
     document.getElementById("roids_pris").innerHTML = Math.floor(roids_pris)
     document.getElementById("belte_pris").innerHTML = Math.floor(belte_pris)
     document.getElementById("sarms_pris").innerHTML = Math.floor(sarms_pris)
+    document.getElementById("sleeves_pris").innerHTML = Math.floor(sleeves_pris)
     document.getElementById("pwo_mengde").innerHTML = Math.floor(boughtpw)
     document.getElementById("protein_mengde").innerHTML = Math.floor(boughtp)
     document.getElementById("airpods_mengde").innerHTML = Math.floor(boughta)
-    document.getElementById("roids_mengde").innerHTML = Math.floor(boughtr)
+    document.getElementById("roids_mengde").innerHTML = Math.floor(boughtroids)
     document.getElementById("belte_mengde").innerHTML = Math.floor(boughtbelt)
     document.getElementById("sarms_mengde").innerHTML = Math.floor(boughtsarms)
+    document.getElementById("sleeves_mengde").innerHTML = Math.floor(boughtsleeves)
 
 }, 10);
 setInterval(function() {
     penger += power
+    formue += power
 
 }, 1000);
-
